93552bcf4034977aa60cb2dd2297f03ad2bb5f78
MohammedAmmarIdris <MohammedAmmarIdris@XEN-LAP-0125>
2023-03-13 17:41:35 +0530

    Exercise 1

---
+++ Exercise 1/src/LambdaExpr.java
@@ -0,0 +1,14 @@
+interface Addable{
+	int add(int a, int b);
+}
+public class LambdaExpr {
+
+	public static void main(String[] args) {
+		Addable ad1=(a,b)->(a+b);
+		System.out.println(ad1.add(10, 20));
+		
+		Addable ad2=(int a,int b)->(a+b);
+		System.out.println(ad1.add(20, 30));
+	}
+
+}
